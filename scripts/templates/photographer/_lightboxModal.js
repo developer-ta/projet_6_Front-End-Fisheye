12d1ec1fbf552a21062e25acacc75f75adeb4880
developer-ta <78349294+developer-ta@users.noreply.github.com>
2024-03-17 00:04:13 +0100

    add close open hendler lightbox modal

--- scripts/templates/photographer/_lightboxModal.js
+++ scripts/templates/photographer/_lightboxModal.js
@@ -1,4 +1,6 @@
 // lightbox part
+
+
 export const _lightboxModal = () => {
 	const $lightBox_centenaire = document.querySelector('.lightbox')
 	const lightBox_template = `  
@@ -10,10 +12,44 @@ export const _lightboxModal = () => {
 		<img class="modal-contend" src="/assets/Sample Photos/Mimi/Travel_Lonesome.jpg">
 	  </div>
 	  <h3 id="title_lightbox">Lonesome</h3>
-  
-	</div>
-  `;
+  	</div>`;
 
 	$lightBox_centenaire.innerHTML = lightBox_template;
+	const $imgList = document.getElementsByClassName("galleryImg-container");
+	const $gallery = document.getElementById('gallery');
+	const $close_span = $lightBox_centenaire.querySelector('.close')
+	// lightbox Handler function 
+	const lightboxHandler = () => {
+		debugger
+		if ($gallery.style.display !== 'none') {
+
+			$gallery.style.display = 'none'
+		} else {
+			$gallery.style.display = 'flex'
+
+		}
+		if ($lightBox_centenaire.style.display !== 'none') {
+			$lightBox_centenaire.style.display = 'block'
+		} else {
+
+			$lightBox_centenaire.style.display = 'none'
+		}
+
+	}
+
+	for (let i = 0; i < $imgList.length; i++) {
+
+		const $element = $imgList[i];
+		if ($element.tagName !== 'VIDEO') {
+			$element.firstChild.identity = { index: i, src: $element.firstChild.src }
+		} else {
+			$element.firstChild.identity = { index: i, src: $element.firstChild.firstChild.src }
+		}
+
+		$element.firstChild.addEventListener("click", lightboxHandler)
+
+	}
+	$close_span.addEventListener("click", lightboxHandler);
+
 
 };
