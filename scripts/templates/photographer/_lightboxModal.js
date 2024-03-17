1da3b66bc44e3d1a4e9a0580b6093a6f74710a62
developer-ta <78349294+developer-ta@users.noreply.github.com>
2024-03-17 10:30:28 +0100

    add close open hendler lightbox modal add list src img

--- scripts/templates/photographer/_lightboxModal.js
+++ scripts/templates/photographer/_lightboxModal.js
@@ -2,23 +2,29 @@
 
 
 export const _lightboxModal = () => {
-	const $lightBox_centenaire = document.querySelector('.lightbox')
+
+	//html 
 	const lightBox_template = `  
 	<div id="dialog" aria-label="image closeup view">
-	  <span class="previous circuit">❮</span>
-	  <span class="next circuit">❯</span>
-	  <span class="close">×</span>
-	  <div class="img-lightbox">
-		<img class="modal-contend" src="/assets/Sample Photos/Mimi/Travel_Lonesome.jpg">
-	  </div>
-	  <h3 id="title_lightbox">Lonesome</h3>
-  	</div>`;
+	<span class="previous circuit">❮</span>
+	<span class="next circuit">❯</span>
+	<span class="close">×</span>
+	<div class="img-lightbox">
+	<img class="modal-contend" src="/assets/Sample Photos/Mimi/Travel_Lonesome.jpg">
+	</div>
+	<h3 id="title_lightbox">Lonesome</h3>
+	</div>`;
 
+
+	//script js
+	//Dom el
+	const $lightBox_centenaire = document.querySelector('.lightbox')
 	$lightBox_centenaire.innerHTML = lightBox_template;
-	const $imgList = document.getElementsByClassName("galleryImg-container");
+	const $imgContainerList = document.getElementsByClassName("galleryImg-container");
 	const $gallery = document.getElementById('gallery');
 	const $close_span = $lightBox_centenaire.querySelector('.close')
-	// lightbox Handler function 
+
+	//function for open or close lightbox
 	const lightboxHandler = () => {
 		debugger
 		if ($gallery.style.display !== 'none') {
@@ -36,20 +42,35 @@ export const _lightboxModal = () => {
 		}
 
 	}
+	//list of src to imgs
+	const _srcList = []
+	//set src img in element Dom
+	for (let i = 0; i < $imgContainerList.length; i++) {
 
-	for (let i = 0; i < $imgList.length; i++) {
+		const $element = $imgContainerList[i];
 
-		const $element = $imgList[i];
-		if ($element.tagName !== 'VIDEO') {
-			$element.firstChild.identity = { index: i, src: $element.firstChild.src }
-		} else {
-			$element.firstChild.identity = { index: i, src: $element.firstChild.firstChild.src }
+		if ($element.tagName !== 'VIDEO' && $element.firstChild.src) {
+
+			$element.firstChild.srcByIndex = $element.firstChild.src
+			_srcList.push($element.firstChild.src)
+			$element.firstChild.srcList = _srcList
+
+		} else if ($element.firstChild.firstChild.src) {
+
+			$element.firstChild.srcByIndex = $element.firstChild.firstChild.src
+			_srcList.push($element.firstChild.firstChild.src)
+			$element.firstChild.srcList = _srcList
 		}
-
+		console.log('$element.firstChild.srcByIndex: ', $element.firstChild.srcByIndex);
+		console.log('$element.firstChild.srcList: ', $element.firstChild.srcList);
+		//action for open lightbox modal
 		$element.firstChild.addEventListener("click", lightboxHandler)
 
+
 	}
+	//action for close lightbox modal
 	$close_span.addEventListener("click", lightboxHandler);
 
+	//console.log('$imgContainerList: ', $imgContainerList[0].firstChild.srcByIndex);
 
 };
